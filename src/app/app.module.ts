c17542ebbafd245a86fdd315bb66583e40662301
Iwo <iwoskater3@hotmail.com>
2022-02-28 11:14:35 +0100

    Angular Life Cycle exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component
   ],
   imports: [
     BrowserModule,
